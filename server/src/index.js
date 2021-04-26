54c22955bc388a947369d11466cdc9efda2015bb
bensvarnum <bens.varnum@gmail.com>
2021-04-25 22:14:20 -0400

    add server for travel-log

---
+++ server/src/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const middlewares = require("./middlewares");
+const logs = require("./api/logs");
+const app = express();
+
+app.use(morgan("common"));
+app.use(helmet());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    msg: "Welcome to the Jungle!",
+  });
+});
+app.use("/api/logs", logs);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);
